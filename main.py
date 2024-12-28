def3fe50af70e5c69f090dd36ac09fdb17a913cd
yuta15 <yxyi8udm1eb1us6aea7s@gmail.com>
2024-12-28 16:34:50 +0900

    API追加

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ app = FastAPI()
 async def test():
     return {'message': 'Hello World'}
 
+@app.get('/test')
+async def test2():
+    return {'message': 'test'}
+
+@app.post('/test2/{item}')
+async def test3(item):
+    return {'message': item}
+
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=80)
